0239326ae3f01102bf25bd6ec4004bab7211d5cb
luiz victor <fs.luizvictor@gmail.com>
2020-07-21 00:04:33 -0300

    fazendo listagem

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const routes = express.Router()
 
 //Configuração do Cassandra
 const database = {
-    contactPoints: ['127.0.0.1'],
+    contactPoints: ['http://localhost:3333'],
     keyspace: 'blog'
 }
 
@@ -15,5 +15,26 @@ connection.connect(() => {
     console.log('Connection established...')
 })
 
+routes.get('/posts', ((req, res) => {
+
+    const data = {
+        "error": 1,
+        "Posts": ""
+    }
+
+    const select = 'SELECT * from posts'
+
+    connection.execute(select, ((err, rows) => {
+        if (rows.length != 0) {
+            data["error"] = 0;
+            data["Books"] = rows;
+            res.json(data);
+        } else {
+            data["Books"] = 'No books Found..';
+            res.json(data);
+        }
+    }))
+}))
 
 
+module.exports = routes
